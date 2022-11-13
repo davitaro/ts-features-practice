8a70e624d18f555dfbb4081ad899658876072723
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-11-13 09:04:46 -0500

    variable annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,27 @@
+let apples: number = 5;
+
+let speed: string = "fast";
+
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+//built-in objects
+let now: Date = new Date();
+
+//arrays
+let colors: string[] = ["red", "green", "blue"];
+
+let myNumbers: number[] = [1, 2, 3, 4];
+
+//class
+class Car {}
+let car: Car = new Car();
+
+//object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
--- types.ts
+++ types.ts
@@ -1,1 +1,9 @@
-const today = new Date();
+const today: Date = new Date();
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+const red = new Color();
