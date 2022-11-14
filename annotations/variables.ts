cdbd5f52c327fbac2674f2a51088478228d3141b
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-11-14 05:54:57 -0500

    annotations with variables, objects, functions

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -25,3 +25,35 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+//function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+//when to use type annotations
+
+// 1) Function that returns "any" type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+//2) delayed initialization
+let words = ["red", "green", "blue"];
+let foundWord = false;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+//3) when inference doesn't work
+let numbers = [-10, -2, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
