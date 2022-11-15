6748cf6e1f7be17ed8b0d025fd02b5d3dd3fc23a
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-11-15 07:03:18 -0500

    interfaces

---
+++ interfaces.ts
@@ -0,0 +1,34 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+interface Reportable {
+  summary(): string;
+}
+
+const oldCivic = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
+
+const myDrink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  },
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(myDrink);
